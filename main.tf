8a0b7d46a2c62a83f8eb77688576ff8254f318b9
pravn98 <praveenkumar.r@bambeeq.com>
2020-11-30 21:16:52 +0530

    test ec2 version6

--- main.tf
+++ main.tf
@@ -23,28 +23,12 @@ provider "aws" {
 
 provider "random" {}
 
-resource "random_pet" "sg" {}
-
 resource "aws_instance" "web" {
-  ami                    = "ami-0a741b782c2c8632d"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
-}
-
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  ami         = "ami-0a741b782c2c8632d"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-f4d3a0ad"
+  key_name = "BQ-EKS"
+  vpc_security_group_ids = "sg-0161c2c1d66785530"
 }
 
 output "web-address" {
